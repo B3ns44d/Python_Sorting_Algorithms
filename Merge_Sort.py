ed992c1756df652367a47ccda5eaacec263b2db5
ro <ro@localhost.localdomain>
2021-03-12 16:47:56 +0900

    Changed 'remove' for 'del' and fixed typos in Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,7 +1,7 @@
 def merge_sort(unsorted_list):
     if len(unsorted_list) <= 1:
         return unsorted_list
-# Find the middle point and devide it
+# Find the middle point and divide the unsorted list
     middle = len(unsorted_list) // 2
     left_list = unsorted_list[:middle]
     right_list = unsorted_list[middle:]
@@ -18,16 +18,16 @@ def merge(left_half,right_half):
     while len(left_half) != 0 and len(right_half) != 0:
         if left_half[0] < right_half[0]:
             res.append(left_half[0])
-            left_half.remove(left_half[0])
+            del left_half[0]
         else:
             res.append(right_half[0])
-            right_half.remove(right_half[0])
+            del right_half[0]
     if len(left_half) == 0:
         res = res + right_half
     else:
         res = res + left_half
     return res
-# you can add any list of number here
+# You can add any list of numbers here
 unsorted_list = [6,5,3,1,8,7,2,4]
 
 print(merge_sort(unsorted_list))
